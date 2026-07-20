29f452d2c59d951614092bb433faee2df409a215
ADIOR-enigma <anubhabd116@gmail.com>
2026-07-20 18:00:02 +0530

    fix: visualizer daemon

--- user_scripts/way_layers/visualizer/visualizer_daemon.py
+++ user_scripts/way_layers/visualizer/visualizer_daemon.py
@@ -10,7 +10,9 @@ import threading
 import gi
 
 gi.require_version("Gtk", "3.0")
+gi.require_version("Gdk", "3.0")
 gi.require_version("GtkLayerShell", "0.1")
+gi.require_version("Pango", "1.0")
 import cairo
 from gi.repository import Gdk, GLib, Gtk, GtkLayerShell, Pango
 
